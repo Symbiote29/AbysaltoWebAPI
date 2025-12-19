8ac27ba8545d2b99d87cb85433cc6e1f7861ef01
Filip Antunović <77288143+Symbiote29@users.noreply.github.com>
2025-12-19 04:02:03 +0100

    Implemented the AddEditCartItem POST method to add items to the cart.
    Implemented the AddEditCartItem POST method to add items to the cart.

--- AbysaltoWebAPI/AbysaltoWebAPI/Controllers/CartController.cs
+++ AbysaltoWebAPI/AbysaltoWebAPI/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AbysaltoWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AbysaltoWebAPI.Controllers
 {
@@ -14,6 +15,56 @@ namespace AbysaltoWebAPI.Controllers
         public CartController(ApiContext context)
         {
             _context = context;
+        }
+
+        [HttpPost("items")]
+        public async Task<IActionResult> AddEditCartItem([FromBody] AddCartItemDTO request)
+        {
+            var cart = await _context.carts.Include(c => c.items).FirstOrDefaultAsync(i => i.userId == request.userId);
+
+            if(cart == null)
+            {
+                cart = new Cart
+                {
+                    cartId = Guid.NewGuid(),
+                    userId = request.userId,
+                    createdAt = DateTime.UtcNow,
+                    updatedAt = DateTime.UtcNow,
+                };
+
+                _context.carts.Add(cart);
+            }
+
+            var existingItem = cart.items.FirstOrDefault(i => i.productId == request.productId);
+
+            if(existingItem != null)
+            {
+                existingItem.itemPrice = request.itemPrice;
+                existingItem.quantity = request.quantity;
+                existingItem.updatedAt = DateTime.UtcNow;
+            }
+            else
+            {
+                var newItem = (new CartItem
+                {
+                    itemId = Guid.NewGuid(),
+                    cartId = cart.cartId,
+                    productId = request.productId,
+                    productName = request.productName,
+                    quantity = request.quantity,
+                    itemPrice = request.itemPrice,
+                    createdAt = DateTime.UtcNow,
+                    updatedAt = DateTime.UtcNow,
+                });
+
+                _context.cartItems.Add(newItem);
+            }
+
+            cart.updatedAt = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+
+            return new JsonResult(Ok(request));
         }
     }
 }
---
+++ AbysaltoWebAPI/AbysaltoWebAPI/Models/AddCartItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AbysaltoWebAPI.Models
+{
+    public class AddCartItemDTO
+    {
+        public int userId { get; set; }
+        public int productId { get; set; }
+        public string productName { get; set; } = string.Empty;
+        public int quantity { get; set; }
+        public decimal itemPrice { get; set; }
+    }
+}
