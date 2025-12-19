030ba93bbcd6cbc916c82ea7925aa5884ba9b5b2
Filip Antunović <77288143+Symbiote29@users.noreply.github.com>
2025-12-19 02:25:28 +0100

    Simplistic db models for shopping cart and shopping cart items.
    Added a Model folder and 2 model classes: Cart and CartItem. These model classes will be used to store data sent from the API to our database.

---
+++ AbysaltoWebAPI/AbysaltoWebAPI/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbysaltoWebAPI.Models
+{
+    public class Cart
+    {
+        [Key]
+        public Guid cartId { get; set; }
+        public int userId { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public List<CartItem> items { get; set; } = new();
+    }
+}
---
+++ AbysaltoWebAPI/AbysaltoWebAPI/Models/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbysaltoWebAPI.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public Guid itemId {  get; set; }
+        public Guid cartId { get; set; }
+        public int productId { get; set; }
+        public String productName { get; set; } = string.Empty;
+        public decimal itemPrice { get; set; }
+        public int quantity { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public Cart cart { get; set; } = new();
+    }
+}
