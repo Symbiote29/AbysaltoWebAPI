70bac1910695529592e7143c2b64ab08ff20a9c3
Filip Antunović <77288143+Symbiote29@users.noreply.github.com>
2025-12-19 03:05:17 +0100

    Added the CartController
    Added the CartController controller, defined and initialized the ApiContext.

---
+++ AbysaltoWebAPI/AbysaltoWebAPI/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using AbysaltoWebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AbysaltoWebAPI.Models;
+
+namespace AbysaltoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CartController(ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
